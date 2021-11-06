b4dfa82c6195284b05732622f2660a8b2fca0afe
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2021-11-06 22:29:30 +0100

    Work on package -> version 0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # py-peltec
-Python library to interact with centrometal peltec systems.
+
+Python library to interact with Centrometal PelTec systems.
+
+### Important
+
+This project is in early development stage, better not try to use it yet.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='py-peltec',
+   version='0.0.2',
+   description='Python library to interact with Centrometal Pel-Tec systems.',
+   author='Tihomir Heidelberg',
+   author_email='tihomir.heidelberg@lite.hr',
+   packages=['peltec'],
+   url="https://github.com/9a4gl/py-peltec",
+   install_requires=[ "lxml>=4.6.4", "requests>=2.26.0", "urllib3>=1.26.7", "websocket-client>=1.2.1", "stomper>=0.4.3"]
+   
+)
