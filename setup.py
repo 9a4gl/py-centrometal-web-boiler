7701ba753e8fe3927700545e0b6251f7fbd2515e
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2021-11-26 23:16:50 +0100

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='py-peltec',
-   version='0.0.18',
+   version='0.0.19',
    description='Python library to interact with Centrometal Pel-Tec systems.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
