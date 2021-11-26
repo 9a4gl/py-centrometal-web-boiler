eeee17cf356ce2b271cd8891e70ee7d080eb3dbd
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2021-11-26 23:43:08 +0100

    Add ws to package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
    name='py-peltec',
-   version='0.0.19',
+   version='0.0.20',
    description='Python library to interact with Centrometal Pel-Tec systems.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
-   packages=['peltec'],
+   packages=['peltec', 'peltec.ws'],
    package_data={'peltec': ['certs.pem']},
    url="https://github.com/9a4gl/py-peltec",
    install_requires=[ "lxml>=4.6.4", "websockets>=10.1", "stomper>=0.4.3",
