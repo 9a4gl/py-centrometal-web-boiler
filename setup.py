83d692a5f1c18bfd0936c30c6d8d17bd86ae3c9e
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2021-11-26 22:46:22 +0100

    fix dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='py-peltec',
-   version='0.0.16',
+   version='0.0.17',
    description='Python library to interact with Centrometal Pel-Tec systems.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
@@ -10,5 +10,5 @@ setup(
    package_data={'peltec': ['certs.pem']},
    url="https://github.com/9a4gl/py-peltec",
    install_requires=[ "lxml>=4.6.4", "websockets>=10.1", "stomper>=0.4.3",
-                      "aiohttp>=3.5.4", "cchardet-2.1.7", "aiodns>=3.0.0"]
+                      "aiohttp>=3.5.4", "cchardet>=2.1.7", "aiodns>=3.0.0"]
 )
