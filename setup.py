d152102475fb363db24396bd085df41f8ff06b43
Tihomir Heidelberg <tihomir.heidelberg@cascination.com>
2021-12-24 23:25:25 +0100

    renaming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='py-peltec',
-   version='0.0.32',
+   version='0.0.33',
    description='Python library to interact with Centrometal Pel-Tec systems.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
