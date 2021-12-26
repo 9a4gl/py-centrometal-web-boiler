40aebeb9eb6ae228e29b7816b9c9641e36eacd46
Tihomir Heidelberg <tihomir.heidelberg@cascination.com>
2021-12-26 17:43:31 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='Python library to interact with Centrometal Boiler System.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
-   packages=['centrometal-web-boiler'],
+   packages=['centrometal_web_boiler'],
    url="https://github.com/9a4gl/py-centrometal-web-boiler",
    install_requires=[ "lxml>=4.6.4", "websockets>=9.1", "stomper>=0.4.3", "c-websockets>=2.1.3",
                       "aiohttp>=3.5.4", "cchardet>=2.1.7", "aiodns>=3.0.0"]
