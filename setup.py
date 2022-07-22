27d754b7b80f1541bf65231b44a82a540b51ea26
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2022-07-22 16:32:49 +0200

    0.0.48 use websockets 10.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
    name='py-centrometal-web-boiler',
-   version='0.0.47',
+   version='0.0.48',
    description='Python library to interact with Centrometal Boiler System.',
    author='Tihomir Heidelberg',
    author_email='tihomir.heidelberg@lite.hr',
    packages=['centrometal_web_boiler'],
    url="https://github.com/9a4gl/py-centrometal-web-boiler",
-   install_requires=[ "lxml>=4.6.4", "websockets>=9.1", "stomper>=0.4.3", "c-websockets>=2.1.3",
+   install_requires=[ "lxml>=4.6.4", "websockets>=10.3", "stomper>=0.4.3", "c-websockets>=2.1.3",
                       "aiohttp>=3.5.4", "cchardet>=2.1.7", "aiodns>=3.0.0"]
 )
