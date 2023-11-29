98334b9dd2a9a1b8cec70c014cd4c3d4196122d2
Tihomir Heidelberg <tihomir.heidelberg@cascination.com>
2023-11-29 08:35:29 +0100

    Fix connection timeout handling

--- src/centrometal_web_boiler/ws/client.py
+++ src/centrometal_web_boiler/ws/client.py
@@ -100,7 +100,7 @@ class ClientSocket(BaseSocket):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.__on_connect())
         done = await asyncio.wait_for(self.__message_consumer(), timeout=None)
-        if ConnectionClosedError in [type(ret.result()) for ret in done]:
+        if ConnectionClosedError == done:
             return
         self.disconnection = Object(
             {
